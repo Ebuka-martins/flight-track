704e1e4398c4897cd8c7cc00f59aea4c40da9d3f
Ebuka-martins <martin4best@gmail.com>
2025-07-06 21:18:57 +0100

    Add and update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/flight-track/', // 👈 this is essential for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
